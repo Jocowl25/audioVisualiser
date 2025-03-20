222ac793c5e6c85d451f80923d3464a85d6adedd
John Doe <jocowl25@gmail.com>
2025-03-19 22:10:05 -0400

    everything

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+console.clear()
+const audioMap = new Map();
+audioMap.set("Sample1.mp3","Sample1.mp3")
+audioMap.set("Sample2.mp3","Sample2.mp3")
 let audio=document.querySelector("audio")
+let select=document.querySelector("select")
+const fileInput = document.querySelector('input[type="file"]');
 audio.play()
 const audioCtx = new AudioContext();
 const source = audioCtx.createMediaElementSource(audio);
@@ -9,26 +15,58 @@ const bufferLength = analyser.frequencyBinCount;
 const timeArray = new Uint8Array(bufferLength);
 const freqArray = new Uint8Array(bufferLength);
 const canvas=document.querySelector("canvas")
+canvas.width=600;
+canvas.height=300;
+const w=canvas.width;
+  const h=canvas.height;
 const canvasCtx=canvas.getContext("2d");
+fileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  canvasCtx.fillStyle = "#060647";
+  canvasCtx.fillRect(0, 0, w, h);
+  select.innerHTML=select.innerHTML+`<option>${file.name}</option>`
+audio.src=url
+select.value=file.name
+audioMap.set(file.name, url);
+audio.play()
+});
+let val=select.value
 function draw(){
+  if(val!=select.value){
+  audio.src=audioMap.get(select.value)
+  audio.play()
+  val=select.value
+  }
     if(audio.paused){
         requestAnimationFrame(draw)
         return;
     }
-    const w=600;
-    const h=300;
     analyser.getByteFrequencyData(freqArray);
     analyser.getByteTimeDomainData(timeArray);
-    canvasCtx.fillStyle = "#b1d4e6";
+    canvasCtx.fillStyle = "#060647";
     canvasCtx.fillRect(0, 0, w, h);
     canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = "#601ec9";
+    canvasCtx.strokeStyle = "#080896";
     canvasCtx.beginPath();
     const sw = w / bufferLength;
-    let x = 0;  
-    for(let i=0;i<bufferLength;i+=2){
+
+let x=0
+let skip=5
+const barWidth = (w / bufferLength)*1.5*skip;
+let barHeight;
+x = 0;
+for (let i = 0; i < bufferLength; i+=skip) {
+    barHeight = freqArray[i];
+    canvasCtx.fillStyle = `rgb(105 25 ${(i/bufferLength) * 115+(255-115)})`;
+    canvasCtx.fillRect(x, (h/2)-barHeight/2, barWidth, barHeight);
+  
+    x += (barWidth);
+  }
+  x = 0;  
+    for(let i=0;i<bufferLength;i++){
     const v = timeArray[i] / 128.0;
-  const y = v * (h / 4);
+  const y = v * (h / 2);
 
   if (i === 0) {
     canvasCtx.moveTo(x, y);
@@ -37,21 +75,9 @@ function draw(){
   }
   x += sw;
     }
-    canvasCtx.lineTo(w, h / 2);
+    canvasCtx.lineTo(w, h);
     canvasCtx.stroke();
     canvasCtx.closePath();
-
-x=0
-const barWidth = (w / bufferLength) * 2.5;
-let barHeight;
-x = 0;
-for (let i = 0; i < bufferLength; i++) {
-    barHeight = freqArray[i] / 2;
-    canvasCtx.fillStyle = `rgb(50 50 ${barHeight + 100})`;
-    canvasCtx.fillRect(x, h, barWidth, barHeight);
-  
-    x += (barWidth + 1);
-  }
     requestAnimationFrame(draw)
 }
 requestAnimationFrame(draw)
